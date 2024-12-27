ecaa8fd5225ef14a9f1907a202d637261b8a5a86
Wesley de Oliveira <wesleysistemaor@gmail.com>
2024-12-27 17:20:59 -0300

    initForm

---
+++ script.js
@@ -0,0 +1,26 @@
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const message = document.getElementById('message').value;
+
+    // Enviar dados para o Google Sheets
+    fetch('https://script.google.com/macros/s/AKfycbw8W6eLQI83TJgTRpjJX2V3i565YJShNSdgJspnvVBiX9VDNJ0wqLLgqLSnb2-fxHUgAg/exec', {
+        method: 'POST',
+        mode: 'cors',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ name, email, message })
+    })
+    .then(response => response.json())
+    .then(data => {
+        if (data.result === 'success') {
+            alert('Formulário enviado com sucesso!');
+        } else {
+            alert('Ocorreu um erro ao enviar o formulário.');
+        }
+    })
+    .catch(error => console.error('Erro:', error));
+});
